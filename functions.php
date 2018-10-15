a30a3654c4508882c344116bf04a2cb98fd3e19e
Developer's Diary <30465804+developerdiary@users.noreply.github.com>
2018-10-15 15:03:27 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,33 @@ Add below code in functions.php file
 */
 
 
+function wpb_set_post_views($postID) {
+    $count_key = 'wpb_post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+//To keep the count accurate, lets get rid of prefetching
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
+
+function wpb_track_post_views ($post_id) {
+    if ( !is_single() ) return;
+    if ( empty ( $post_id) ) {
+        global $post;
+        $post_id = $post->ID;    
+    }
+    wpb_set_post_views($post_id);
+}
+add_action( 'wp_head', 'wpb_track_post_views');
+
+
 function the_new_popular_post_feed() {
     add_feed('popularpost', 'a_new_feed');
 }
